a991aa36cbb93a6852426d7ece80a3564f438acc
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-11-27 23:31:32 +0100

    add consolelog for queryselector

---
+++ index.js
@@ -0,0 +1,6 @@
+function print(){ console.log("Congatz, programmers gets a free beer at the bar, mortals will have to pay!") }
+
+var video = document.querySelector('.video');
+video.addEventListener("ended", print);
+
+
